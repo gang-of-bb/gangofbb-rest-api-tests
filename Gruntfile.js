0eff2156a1b6644b7ae8247aa5d96ef990176250
Benjamin Heintz <heintz.benjamin@gmail.com>
2013-12-10 16:34:16 +0100

    Adding open browser grunt task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
     var coffee = require('./grunt_tasks/coffee');
     var shell = require('./grunt_tasks/shell');
+    var open = require('./grunt_tasks/open');
 
     /**
      * grunt configuration.
@@ -17,7 +18,12 @@ module.exports = function(grunt) {
         /**
          * run task
          */
-        shell: shell
+        shell: shell,
+
+        /**
+         * open browser task
+         */
+        open: open
     });
 
     /**
@@ -25,11 +31,12 @@ module.exports = function(grunt) {
      */
     grunt.loadNpmTasks('grunt-contrib-coffee');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-open');
 
     /**
      * register grunt tasks.
      */
     grunt.registerTask('build', ['coffee']);
-    grunt.registerTask('debug', ['build', 'shell:run']);
+    grunt.registerTask('debug', ['build', 'shell:run', 'open']);
     grunt.registerTask('run', ['shell:run']);
 };
---
+++ grunt_tasks/open.js
@@ -0,0 +1,5 @@
+module.exports = {
+    browser : {
+      path : 'http://gangofbb.bhtz.fr/documentation'
+    }
+};
